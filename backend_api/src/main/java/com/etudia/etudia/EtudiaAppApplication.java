10a1d5f86d2903807a1d9dac69253907e7ab8e76
laanatri <laanatri.ade@gmail.com>
2025-07-16 13:03:20 +0200

    init spring

---
+++ backend_api/src/main/java/com/etudia/etudia/EtudiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.etudia.etudia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudiaAppApplication.class, args);
+	}
+
+}
---
+++ backend_api/src/test/java/com/etudia/etudia/EtudiaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etudia.etudia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
