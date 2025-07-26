2ca9446356f28d7e79d5a178e3e0d704c6c05552
laanatri <laanatri.ade@gmail.com>
2025-07-26 09:35:50 +0200

    update cors policy

--- backend_api/src/main/java/com/etudia/etudia/security/SecurityConfig.java
+++ backend_api/src/main/java/com/etudia/etudia/security/SecurityConfig.java
@@ -69,8 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
