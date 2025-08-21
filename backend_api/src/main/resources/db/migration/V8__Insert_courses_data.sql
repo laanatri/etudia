3b4904630ec3b92ddcb9e728a80b9a98ccba1eab
laanatri <laanatri.ade@gmail.com>
2025-08-21 15:12:02 +0200

    add migrations insert courses, blocs and flashcards

---
+++ backend_api/src/main/resources/db/migration/V8__Insert_courses_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO courses (name, course_url, user_id)
+VALUES (E'Conception et patrons d\'architectures', 'https://yydyzkwmmyhlvhlrhsbx.supabase.co/storage/v1/object/public/courses//conception_et_patrons_darchitectures.rtf', 3),
+       ('Architecture web', 'https://yydyzkwmmyhlvhlrhsbx.supabase.co/storage/v1/object/public/courses//conception_et_patrons_darchitectures.rtf', 3)
---
+++ backend_api/src/main/resources/db/migration/V9__Insert_blocs_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO blocs (name, themes, user_id, course_id)
+VALUES (E'Conception et patrons d''architectures', 'architecture,patrons,stack technique,design logiciel', 3, 1),
+('Architecture web', 'web,architecture,frontend,backend,frameworks', 3, 2)
