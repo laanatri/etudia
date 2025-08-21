19eae90fb9a7a52d57a530c0e9cda86ba09824b3
laanatri <laanatri.ade@gmail.com>
2025-08-21 14:49:24 +0200

    update model User

--- backend_api/src/main/java/com/etudia/etudia/model/User.java
+++ backend_api/src/main/java/com/etudia/etudia/model/User.java
@@ -1,19 +1,23 @@
 package com.etudia.etudia.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
-//@DiscriminatorColumn(name = "role")
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -29,11 +33,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-
     @Column(name = "created_at", insertable = false, updatable = false)
     private OffsetDateTime createdAt;
 
     @Column(name = "updated_at", insertable = false, updatable = false)
     private OffsetDateTime updatedAt;
-
 }
