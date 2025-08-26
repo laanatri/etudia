6852b75bf1d30ce2e9d28d9ea2cf5fafebf9e3ff
laanatri <laanatri.ade@gmail.com>
2025-08-26 15:30:58 +0200

    update button form style

--- frontend_web/src/app/components/ui/ButtonForm.tsx
+++ frontend_web/src/app/components/ui/ButtonForm.tsx
@@ -7,7 +7,7 @@ export default function ButtonForm({disabled, text}: ButtonProps) {
     return (
         <button
             type="submit"
-            className={`mt-4 mx-auto w-full md:max-w-80 btn btn-accent rounded-full text-xl font-normal ${disabled ? "btn-disabled" : ""} `}
+            className={`mt-4 mx-auto w-full md:max-w-100 btn btn-accent rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] ${disabled ? "btn-disabled" : ""} `}
             aria-disabled={disabled}>
             {text}
         </button>
