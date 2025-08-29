bf54e600b192db65453830f2a5dd8052597a7664
laanatri <laanatri.ade@gmail.com>
2025-08-29 10:43:44 +0200

    update style for pwa

--- frontend_web/src/app/login/page.tsx
+++ frontend_web/src/app/login/page.tsx
@@ -1,18 +1,23 @@
+'use client';
+
 import LoginForm from "@/app/components/forms/LoginForm";
 import {Suspense} from "react";
 import fonts from "./../utils/fonts";
 import ButtonLink from "@/app/components/ui/ButtonLink";
+import { usePWA } from "../PWAContext";
 
 
 export default function Login() {
+    const isPWA = usePWA();
+
     return (
-        <main className="pt-17 px-4 pb-10 min-h-screen flex justify-center flex-col items-center">
+        <main className={`px-4 ${isPWA ? "py-10" : "pt-17 pb-10"} min-h-screen max-w-100 mx-auto flex justify-center flex-col items-center`}>
             <h1 className="text-center text-2xl">Connexion</h1>
             <Suspense>
                 <LoginForm/>
             </Suspense>
             <div className="divider divider-vertical divider-primary">OU</div>
-            <div className={`${fonts.openSans.className}`}>
+            <div className={`${fonts.openSans.className} w-full`}>
                 <p className="text-center text-sm">Tu n'as pas encore de compte&nbsp;? <br/>
                     alors inscrit toi et créer tes premières capsules&nbsp;!
                 </p>
