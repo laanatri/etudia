16b3ea4b8af4b39e3714882357039b0b1790a428
laanatri <laanatri.ade@gmail.com>
2025-08-29 15:19:39 +0200

    clean navbar

--- frontend_web/src/app/components/layout/Navbar.tsx
+++ frontend_web/src/app/components/layout/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { usePWA } from "@/app/PWAContext";
-import {Home, LogIn, LogOut, User} from "lucide-react";
-import {useSession, signOut} from "next-auth/react";
+import { LogIn, User} from "lucide-react";
+import {useSession} from "next-auth/react";
 import Link from "next/link";
 import {useEffect, useState} from "react";
 
@@ -18,14 +18,8 @@ export default function Navbar() {
         setMounted(true);
     }, []);
 
-    console.log("YOYOYOYOYOOYOY")
-    console.log(session)
-    console.log(status)
-    console.log(isAuthenticated)
-    console.log(isPWA)
-
-    if (!mounted || status === 'loading') {
-        return (
+    return isPWA ? null : (
+        !mounted || status === 'loading' || !isAuthenticated ? (
             <div className="navbar bg-base-100 shadow-sm p-2">
                 <div className="flex-1">
                     <Link href={"/"} className="btn btn-ghost text-xl">Etud IA</Link>
@@ -50,60 +44,30 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
+        ) : (
+            <nav className="navbar bg-base-100 shadow-sm p-2 fixed top-0 z-20">
+                <div className="flex-1">
+                    <Link href={"/"} className="btn btn-ghost text-xl">Etud IA</Link>
+                </div>
+                <div className="flex-none gap-4">
+                    {session?.user?.role === 'ROLE_ADMIN' && (
+                        <Link href="/dashboard/admin" className="hover:text-gray-400">
+                            Dashboard
+                        </Link>
+                    )}
+                    {session?.user?.role === 'ROLE_ETUDIANT' && (
+                        <Link href="/dashboard/etudiant/compte" className="flex items-center gap-2 hover:bg-base-200 p-2 rounded-lg transition-colors" >
+                            <div className="w-8 h-8 rounded-full overflow-hidden">
+                                <img
+                                    alt="Photo de profil"
+                                    src={"https://rugby.vlaanderen/wp-content/uploads/2018/03/Anonymous-Profile-pic.jpg"}
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
+                        </Link>
+                    )}
+                </div>
+            </nav>
         )
-    }
-
-    return (
-        <nav className="navbar bg-base-100 shadow-sm p-2 fixed top-0 z-20">
-            <div className="flex-1">
-                <Link href={"/"} className="btn btn-ghost text-xl">Etud IA</Link>
-            </div>
-
-            <div className="flex-none gap-4">
-
-                {!isAuthenticated ? (
-                    <div className="flex items-center gap-2">
-                        <Link
-                            href="/login"
-                            className="btn btn-ghost rounded-xl btn-sm flex items-center gap-2"
-                        >
-                            <LogIn size={16}/>
-                            Connexion
-                        </Link>
-                        <span className="text-gray-400">|</span>
-                        <Link
-                            href="/register"
-                            className="btn btn-primary rounded-xl btn-sm flex items-center gap-2"
-                        >
-                            <User size={16}/>
-                            Inscription
-                        </Link>
-                    </div>
-                ) : null}
-
-                {isAuthenticated ? (
-                    <>
-                        {session?.user?.role === 'ROLE_ADMIN' && (
-                            <Link href="/dashboard/admin" className="hover:text-gray-400">
-                                Dashboard
-                            </Link>
-                        )}
-                        {session?.user?.role === 'ROLE_ETUDIANT' && (
-                            <Link href="/dashboard/etudiant/compte" className="flex items-center gap-2 hover:bg-base-200 p-2 rounded-lg transition-colors" >
-                                <div className="w-8 h-8 rounded-full overflow-hidden">
-                                    <img
-                                        alt="Photo de profil"
-                                        src={"https://rugby.vlaanderen/wp-content/uploads/2018/03/Anonymous-Profile-pic.jpg"}
-                                        className="w-full h-full object-cover"
-                                    />
-                                </div>
-                                {!isPWA && <span className="text-sm font-medium">{session?.user?.username}</span>}
-                            </Link>
-                        )}
-                    </>
-                ) : null}
-
-            </div>
-        </nav>
     )
 }
