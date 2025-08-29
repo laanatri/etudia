f9951318c95177782cb5bb0f298ebf87b15b84a7
laanatri <laanatri.ade@gmail.com>
2025-08-29 10:44:17 +0200

    update style for pwa

--- frontend_web/src/app/register/page.tsx
+++ frontend_web/src/app/register/page.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import {Suspense} from "react";
 import RegisterForm from "@/app/components/forms/RegisterForm";
 import fonts from "@/app/utils/fonts";
 import ButtonLink from "@/app/components/ui/ButtonLink";
+import { usePWA } from "../PWAContext";
 
 export default function Register() {
+    const isPWA = usePWA();
+
     return (
-        <main className="pt-22 px-4 pb-10 min-h-screen flex justify-center flex-col items-center">
+        <main className={`px-4 ${isPWA ? "py-10" : "pt-22 pb-10"} min-h-screen max-w-100 mx-auto flex justify-center flex-col items-center`}>
             <h1 className="text-center text-2xl">Inscription</h1>
             <Suspense>
                 <RegisterForm/>
             </Suspense>
             <div className="divider divider-vertical divider-primary">OU</div>
-            <div className={`${fonts.openSans.className}`}>
+            <div className={`${fonts.openSans.className} w-full`}>
                 <p className="text-center text-sm">Tu as déjà un compte&nbsp;? <br/>
                     Connecte toi pour continuer d'apprendre&nbsp;!
                 </p>
