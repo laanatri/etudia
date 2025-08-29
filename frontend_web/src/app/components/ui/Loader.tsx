a234661a52a60273eeb227b3250ea5f54d985976
laanatri <laanatri.ade@gmail.com>
2025-08-29 15:01:03 +0200

    add props classSup and update style

--- frontend_web/src/app/components/ui/Loader.tsx
+++ frontend_web/src/app/components/ui/Loader.tsx
@@ -1,12 +1,13 @@
 interface LoaderProps {
     isLoading: boolean;
+    classSup?: string
 }
 
-export default function Loader({ isLoading }: LoaderProps) {
+export default function Loader({ isLoading, classSup = "fixed" }: LoaderProps) {
     if (!isLoading) return null;
 
     return (
-        <div className={`fixed inset-0 z-10 bg-white flex justify-center items-center`}>
+        <div className={`inset-0 z-10 flex justify-center items-center p-10 bg-base-300 ${classSup}`}>
             <span className="loading loading-spinner loading-xl text-secondary"></span>
         </div>
     )
