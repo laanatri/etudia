bce12f07dadea76ccaf1af1d48935ef15e4b5799
laanatri <laanatri.ade@gmail.com>
2025-08-29 15:01:46 +0200

    update style

--- frontend_web/src/app/components/ui/ButtonLink.tsx
+++ frontend_web/src/app/components/ui/ButtonLink.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export default function ButtonLink({href, text}: ButtonProps) {
     return (
-        <Link href={href} className={`mt-4 mx-auto w-full md:max-w-80 btn btn-primary rounded-full text-xl font-normal`}>
+        <Link href={href} className={`mt-4 mx-auto w-full md:max-w-100 btn btn-primary rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)]`}>
             {text}
         </Link>
     )
