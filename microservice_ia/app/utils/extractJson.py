bf0326e40e8849bf8635dd1e2c74f9324919ba48
laanatri <laanatri.ade@gmail.com>
2025-09-08 10:35:58 +0200

    update fonction to check content of response

--- microservice_ia/app/utils/extractJson.py
+++ microservice_ia/app/utils/extractJson.py
@@ -1,12 +1,38 @@
 import json
-
-from typing import List, Dict
+import re
+from typing import List, Dict, Any
 
 def extract_json(text: str) -> List[Dict[str, str]]:
+
+    # Nettoyage pour prédire les réponses possibles de chatGPT
+
+    # regex pour netoyer ce qu'il y aurait autour du json
+    cleaned_text = re.sub(r"```(?:json)?\s*|\s*```", "", text).strip()
+
+    # parser le json
     try:
-        parsed = json.loads(text)
-        if isinstance(parsed, List):
-            return parsed
+        parsed: Any = json.loads(cleaned_text)
     except Exception:
-        pass
-    raise ValueError("Aucun tableau JSON valide trouvé dans la réponse de l'IA")
+        try:
+            parsed = json.loads(cleaned_text.replace("'", '"'))
+        except Exception:
+            raise ValueError("Réponse IA non-JSON")
+
+    # si parsed_json est encore une string si double-encodé
+    if isinstance(parsed_json, str):
+        try:
+            parsed_json = json.loads(parsed_json)
+        except Exception as err:
+            raise ValueError(f"JSON double-encodé invalide: {err}")
+        
+    # lever des erreurs si ça ne correspond pas à la demande
+    if not isinstance(parsed_json, dict):
+        raise ValueError("Format invalide : attendu un objet JSON {\"title\",\"themes\",\"flashcards\"}.")
+    
+    if "title" not in parsed_json or "themes" not in parsed_json or "flashcards" not in parsed_json:
+        raise ValueError("Objet JSON manquant 'title', 'themes' ou 'flashcards'.")
+    
+    if not isinstance(parsed_json["flashcards"], list):
+        raise ValueError("Format inattendu")
+        
+    return parsed_json
