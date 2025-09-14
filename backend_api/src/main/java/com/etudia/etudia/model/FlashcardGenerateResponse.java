8986ba3e522f71d338ad70c14d11785af1ae36f1
laanatri <laanatri.ade@gmail.com>
2025-09-14 09:39:18 +0200

    add FlascardGenerateResponse model received from microservice ia with generated flashcards

---
+++ backend_api/src/main/java/com/etudia/etudia/model/FlashcardGenerateResponse.java
@@ -0,0 +1,18 @@
+package com.etudia.etudia.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class FlashcardGenerateResponse {
+
+    private String title;
+    private String themes;
+    private List<Map<String, String>> flashcards;
+    private Map<String, String> meta;
+
+}
