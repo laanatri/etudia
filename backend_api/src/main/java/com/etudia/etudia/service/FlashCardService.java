41fd9aa7ce2ee400a7df9bfc54b40ff045a13b27
laanatri <laanatri.ade@gmail.com>
2025-09-14 10:15:04 +0200

    add saveFlashcards() to save flashcards in BDD

--- backend_api/src/main/java/com/etudia/etudia/service/FlashCardService.java
+++ backend_api/src/main/java/com/etudia/etudia/service/FlashCardService.java
@@ -1,11 +1,15 @@
 package com.etudia.etudia.service;
 
+import com.etudia.etudia.model.Bloc;
 import com.etudia.etudia.model.FlashCard;
+import com.etudia.etudia.model.FlashcardGenerateResponse;
 
 import java.util.List;
 
 public interface FlashCardService {
 
     List<FlashCard> getFlashCardsByBlocId(Integer blocId);
+
+    boolean saveFlashCards(FlashcardGenerateResponse response, Bloc bloc);
 
 }
--- backend_api/src/main/java/com/etudia/etudia/service/FlashCardServiceImpl.java
+++ backend_api/src/main/java/com/etudia/etudia/service/FlashCardServiceImpl.java
@@ -1,21 +1,57 @@
 package com.etudia.etudia.service;
 
+import com.etudia.etudia.model.Bloc;
 import com.etudia.etudia.model.FlashCard;
+import com.etudia.etudia.model.FlashcardGenerateResponse;
+import com.etudia.etudia.repository.BlocRepository;
 import com.etudia.etudia.repository.FlashCardRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class FlashCardServiceImpl implements FlashCardService {
 
+    private final BlocRepository blocRepository;
     private FlashCardRepository flashCardRepository;
 
     @Override
     public List<FlashCard> getFlashCardsByBlocId(Integer blocId) {
         return flashCardRepository.findByBlocId(blocId);
     }
+
+    @Override
+    @Transactional
+    public boolean saveFlashCards(FlashcardGenerateResponse response, Bloc bloc) {
+        if (response == null || response.getFlashcards() == null || response.getFlashcards().isEmpty()) {
+            return false;
+        }
+
+        try {
+
+            List<FlashCard> flashCards = new ArrayList<>();
+
+            for (Map<String, String> cardData : response.getFlashcards()) {
+                FlashCard flashCard = new FlashCard();
+                flashCard.setTitle(cardData.get("question"));
+                flashCard.setContent(cardData.get("answer"));
+                flashCard.setBloc(bloc);
+                flashCards.add(flashCard);
+            }
+
+            flashCardRepository.saveAll(flashCards);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    };
 
 }
