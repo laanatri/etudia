001abde9ad31217eb4e84427dedcaa6294ed53c8
laanatri <laanatri.ade@gmail.com>
2025-09-14 10:50:09 +0200

    update migrations

--- backend_api/src/main/resources/db/migration/V5__Create_blocs_table.sql
+++ backend_api/src/main/resources/db/migration/V5__Create_blocs_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE blocs
 (
-    id serial,
-    name varchar(255) not null,
-    themes varchar(255),
-    user_id integer not null,
-    course_id integer not null,
+    id         serial,
+    name       varchar(255) not null,
+    themes     varchar(255),
+    user_id    integer      not null,
+    course_id  integer      not null,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     primary key (id),
     constraint fk_blocs_user foreign key (user_id) references users (id) on delete cascade,
