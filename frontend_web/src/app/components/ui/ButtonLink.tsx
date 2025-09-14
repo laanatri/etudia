ca8d4e4c5c9694e624b06179bb8afb3fb85c9ca3
laanatri <laanatri.ade@gmail.com>
2025-09-14 11:25:00 +0200

    update ButtonLink to handle more classes and icons

--- frontend_web/src/app/components/ui/ButtonLink.tsx
+++ frontend_web/src/app/components/ui/ButtonLink.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { ReactElement } from "react";
 
 interface ButtonProps {
     href: string;
     text: string;
+    icon?: ReactElement<any, any>;
+    classSup?: string;
 }
 
-export default function ButtonLink({href, text}: ButtonProps) {
+export default function ButtonLink({href, text, icon, classSup}: ButtonProps) {
     return (
-        <Link href={href} className={`mt-4 mx-auto w-full md:max-w-100 btn btn-primary rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)]`}>
+        <Link href={href} className={`mt-4 mx-auto md:max-w-100 btn btn-primary rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] ${classSup ? classSup : "w-full"}`}>
+            {icon && icon}
             {text}
         </Link>
     )
