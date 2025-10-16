4e96291822854c36f7477db941699acf03c50fdb
laanatri <laanatri.ade@gmail.com>
2025-10-16 09:07:12 +0200

    fix(summaries and quizzies): update capsules output models for gpt response to be validate

--- microservice_ia/app/models/capsules_output_models.py
+++ microservice_ia/app/models/capsules_output_models.py
@@ -5,17 +5,17 @@ from typing import Annotated, List, Optional
 # lève une Exception si non conforme
 
 class Flashcard(BaseModel):
-    question: Annotated[str, Field(ge=1, le=150)]
-    answer: Annotated[str, Field(ge=1, le=500)]
+    question: Annotated[str, Field(min_length=1, max_length=150)]
+    answer: Annotated[str, Field(min_length=1, max_length=500)]
 
 class QuizQuestion(BaseModel):
-    question: Annotated[str, Field(ge=1, le=120)]
+    question: Annotated[str, Field(min_length=1, max_length=120)]
     answers: Annotated[List[str], Field(min_length=4, max_length=4)]
     correct_answer: Annotated[int, Field(ge=0, le=3)]
 
 # ce que je reçois de GPT
 class CapsulesOutput(BaseModel):
-    title: Annotated[str, Field(ge=1, le=100)]
+    title: Annotated[str, Field(min_length=1, max_length=100)]
     themes: str
     flashcards: Optional[List[Flashcard]] = None
     summary: Optional[Annotated[str, Field(max_length=5000)]] = None
