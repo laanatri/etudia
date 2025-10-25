8f10f317149d42f1251bf34556e0a37b15ba1603
laanatri <laanatri.ade@gmail.com>
2025-10-25 19:54:56 +0200

    refactor(front cleaning): add prop classSup to ButtonForm

--- frontend_web/src/app/components/ui/ButtonForm.tsx
+++ frontend_web/src/app/components/ui/ButtonForm.tsx
@@ -1,13 +1,14 @@
 interface ButtonProps {
     disabled: boolean;
     text: string;
+    classSup?: string;
 }
 
-export default function ButtonForm({disabled, text}: ButtonProps) {
+export default function ButtonForm({disabled, text, classSup}: ButtonProps) {
     return (
         <button
             type="submit"
-            className={`mt-4 mx-auto w-full md:max-w-100 btn btn-accent rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] ${disabled ? "btn-disabled" : ""} `}
+            className={`mt-4 mx-auto w-full md:max-w-100 btn btn-accent rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] ${disabled ? "btn-disabled" : ""} ${classSup}`}
             aria-disabled={disabled}>
             {text}
         </button>
