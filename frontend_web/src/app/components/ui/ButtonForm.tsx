fc9e06e84cc00aa1a0c653c6b8290d99c011ba5b
laanatri <laanatri.ade@gmail.com>
2025-10-27 21:24:55 +0100

    feat(delete user): update buttonForm to have an icon

--- frontend_web/src/app/components/ui/ButtonForm.tsx
+++ frontend_web/src/app/components/ui/ButtonForm.tsx
@@ -1,15 +1,19 @@
+import { ReactElement } from "react";
+
 interface ButtonProps {
     disabled: boolean;
     text: string;
     classSup?: string;
+    icon?: ReactElement<any, any>;
 }
 
-export default function ButtonForm({disabled, text, classSup}: ButtonProps) {
+export default function ButtonForm({disabled, text, classSup, icon}: ButtonProps) {
     return (
         <button
             type="submit"
             className={`mt-4 mx-auto w-full md:max-w-100 btn btn-accent rounded-full text-xl font-normal text-black border border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] ${disabled ? "btn-disabled" : ""} ${classSup}`}
             aria-disabled={disabled}>
+            {icon && icon}
             {text}
         </button>
     )
