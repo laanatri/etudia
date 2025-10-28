731c0f7af1e3abf55b5c3a157af2683504d89f8b
laanatri <laanatri.ade@gmail.com>
2025-10-28 10:59:19 +0100

    feat(quizzes): add game request dto

--- backend_api/src/main/java/com/etudia/etudia/controller/GameController.java
+++ backend_api/src/main/java/com/etudia/etudia/controller/GameController.java
@@ -1,8 +1,10 @@
 package com.etudia.etudia.controller;
 
+import com.etudia.etudia.dto.GameRequestDto;
 import com.etudia.etudia.service.GameService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,8 @@ public class GameController {
     private GameService gameService;
 
     @PostMapping("/create")
-    public Integer createGame(Integer score, Integer quizId) {
-        return gameService.createGame(score, quizId);
+    public Integer createGame(@RequestBody GameRequestDto request) {
+        return gameService.createGame(request.getScore(), request.getQuizId());
     }
 
 }
